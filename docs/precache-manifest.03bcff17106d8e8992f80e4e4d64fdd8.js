40185bc37b72bb7ba9b4994d222ecf4aeb9fc2d6
mramos <mramosm@gmail.com>
2020-06-26 19:31:14 -0500

    Static build files

---
+++ docs/precache-manifest.03bcff17106d8e8992f80e4e4d64fdd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb423556dc8d7558a15fcc6105dcbb2d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aead9110718d1c72abb2",
+    "url": "/static/css/main.c64e5487.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aead9110718d1c72abb2",
+    "url": "/static/js/main.caf7b1fb.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
